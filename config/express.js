1cd2bf8e7fd750d01a38b055d0e8019211d8e0fe
Tony <coolhzb@163.com>
2015-03-10 19:03:51 +0800

    add the express.js ...

---
+++ config/express.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
+
+var env = process.env.NODE_ENV || 'development';
+
+/**
+ * Expose
+ *
+ */
+module.exports = function(app, config) {
+
+}
