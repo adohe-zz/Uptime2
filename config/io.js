270953d4db09deee2f27ea22a6a9a2d94e2c0958
Tony <coolhzb@163.com>
2015-03-11 18:20:53 +0800

    enable socket.io ...

--- config/io.js
+++ config/io.js
@@ -1,9 +1,15 @@
-var socketIo = require('socket.io');
-
 var env = process.env.NODE_ENV || 'development';
 /**
  * Expose
  *
  */
 module.exports = function(server, config) {
+
+  var io = require('socket.io')(server);
+
+  io.on('connection', function(socket){
+    socket.on('set check', function(check){
+      socket.set('check', check);
+    });
+  });
 }
