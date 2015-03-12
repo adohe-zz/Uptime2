3c6aee841127b81416c47dc1f75f9ab2b4a327fe
Tony <coolhzb@163.com>
2015-03-12 16:59:34 +0800

    app.js enable the express&socket.io ...

--- app.js
+++ app.js
@@ -3,15 +3,35 @@
  *
  */
 var http = require('http'),
+    url = require('url')
     express = require('express'),
-    mongoose = require('mongoose'),
     config = require('config');
+
+var mongoose = require('./bootstrap');
 
 var app = express(),
     server = http.createServer(app);
 
-var connect = function() {
+// Bootstrap application settings
+require('./config/express')(app, config);
 
+// Bootstrap socket.io settings
+require('./config/io')(server, config);
+
+if(!module.parent) {
+  var serverUrl = url.parse(config.url);
+  var port = serverUrl.port;
+  if(port === null) {
+    port = 80;
+  }
+
+  var host = serverUrl.hostname;
+  server.listen(port, function() {
+    console.log('Express server listening on host %s, port %d', host, port);
+  });
+  server.on('error', function() {
+  });
 }
+
 // Expose
 module.exports = app;
